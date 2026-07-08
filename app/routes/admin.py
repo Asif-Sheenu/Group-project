512b9dc9116c74105b9e67aceb53db8bb4ee1b7b
Asif <asifssheenu@gmail.com>
2026-07-08 12:19:57 +0530

    cat and dog details

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.core.database import get_db
-from app.services.admin import get_all_users, get_all_pets
+from app.services.admin import get_all_users, get_all_dogs,get_all_cats
 
 router = APIRouter()
 
@@ -12,6 +12,10 @@ def fetch_all_users(db: Session = Depends(get_db)):
     return get_all_users(db)
 
 
-@router.get("/pets")
+@router.get("/dogs")
 def fetch_all_pets(db: Session = Depends(get_db)):
-    return get_all_pets(db)
+    return get_all_dogs(db)
+
+@router.get("/cats")
+def fetch_all_pets(db: Session = Depends(get_db)):
+    return get_all_cats(db)
